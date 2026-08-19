b3f9d773528923cb571c36b34948aa73a209fef8
Aamir Hussain <aamir.fss22@gmail.com>
2026-08-19 18:34:46 +0500

    Expose Content-Disposition in CORS to fix download filename

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,8 @@ app.use(cors({
   origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  exposedHeaders: ['Content-Disposition']
 }));
 app.options('*', cors());
 app.use(express.json());
